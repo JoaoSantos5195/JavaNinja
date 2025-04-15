0ea7a839b7840a21235bc91f9e221128ef49046a
JoaoSantos5195 <jotape5195@gmail.com>
2025-04-15 20:15:35 -0300

    Estruturas de repetição e ternários

---
+++ src/Condicoes/LacosDeRepeticao.java
@@ -0,0 +1,18 @@
+package Condicoes;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+
+        int min = 0;
+        int max = 10;
+        while(min < max){
+            min++;
+            System.out.println("Naruto criou " + min + " clones da sombra");
+        }
+        System.out.println("=======================");
+        System.out.println("=======================");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Naruto criou " + i + " clones da sombra");
+        }
+    }
+}
---
+++ src/Condicoes/Ternarios.java
@@ -0,0 +1,17 @@
+package Condicoes;
+
+import java.util.Scanner;
+
+public class Ternarios {
+    public static void main(String[] args) {
+        /*Variável = (condição) ? valorSeVerdadeiro : valorSeFalso*/
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quantas missões você completou? ");
+        short missoes = scanner.nextShort();
+        scanner.close();
+
+        String resultado = (missoes >= 12) ? "Você é avançado" : "Você está no caminho certo";
+        System.out.println(resultado);
+    }
+}
